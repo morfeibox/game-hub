8482e84d4579145b6ccb10698aa8308622cb5726
Atanas Kolev Beychev <a.beychev@hotmail.com>
2024-04-06 14:22:45 +0300

    add Emojies

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -5,6 +5,7 @@ import PlatformIconList from "./PlatformIconList";
 import CriticScore from "./CriticScore";
 import getCroppedImageUrl from "../services/image-url";
 import GameCardContainer from "./GameCardContainer";
+import Emoji from "./Emoji";
 
 interface Props {
     game: Game
@@ -22,6 +23,7 @@ const GameCard = ({game}: Props) => {
                         <CriticScore score={game.metacritic}/>
                     </HStack>
                     <Heading fontSize='2xl' marginBottom={3}>{game.name}</Heading>
+                    <Emoji  rating={game.rating_top} />
                 </CardBody>
             </Card>
         </GameCardContainer>
--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -3,17 +3,18 @@ import {Genre} from "./useGenres";
 import {GameQuery} from "../App";
 
 export interface Platform {
-    id: number,
-    name: string
+    id: number;
+    name: string;
     slug: string
 }
 
 export interface Game {
-    id: number,
-    name: string,
-    background_image: string,
-    parent_platforms: { platform: Platform }[],
-    metacritic: number
+    id: number;
+    name: string;
+    background_image: string;
+    parent_platforms: { platform: Platform }[];
+    metacritic: number;
+    rating_top: number;
 }
 
 const useGames = (gameQuery: GameQuery) => useData<Game>('/games', {
